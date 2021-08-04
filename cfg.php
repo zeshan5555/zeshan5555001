03ab93775c77158165ee7479a8bd11fb804e6528
zeshan5555 <88017843+zeshan5555@users.noreply.github.com>
2021-08-04 05:52:40 +0500

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,26 @@
+<?php
+
+/* data mining */
+
+$sign = "P1FCIY1cCNiLKt5rDLjjEDK8YekIhf24AF5rb5ZPKb0SK9ascB+VLP8mG99C7rdpmUQpke08yVvcvP7I";
+$auth = "Bearer gAAAAABhCJ34gb9Uwi00MFLtQGPeI0BucYITrRyM5xk5WG4d-EkBeNs6KJPTrMNAqIzgEr7XJsrAy0nyfFfcd8C83k11H4U8muvEsTGBjQ5UpTRpeqAnQ5U=";
+
+/* data email */
+
+$sign1 = "2piKFzjuB4bRCUdI7LUXWQ/fDNsiVvH6x8hrRJBIRQ/O+XvZiydU7D4bTOYqYfFS7u/P9HDMuKVQTY2S";
+$auth1 = "Bearer gAAAAABhCJ4bykZ5Jf2i47G94Yppr9zIKDsm8VYphiUr0IBaBjLe3lRPz3Z15nFsU9H0Yv0RKB6b8VIEA0WW1efjK03di5n2QWtq3jv7sZtNEcZdh1jT26A=";
+
+
+/* cookie */
+
+$user_agent = "Mozilla/5.0 (Linux; Android 10; Nokia 2.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.114 Mobile Safari/537.36";
+$ct_ts = "1627954715";
+$token = "KYcLWHApxah7LFsehDFM2tvIRnYpKW6m1zs1Q6SOyaub1apv5pbMq7CS3XG0JDJL";
+$session = "2nfjdzm50bnwfmsfhfj82p9yopaujzn4";
+$bid = "777f623c-f3b6-11eb-bf16-02420a03021e";
+$ct_v = "4.1.70";
+$ct_av = "2.0.0.208";
+
+
+
+?>
